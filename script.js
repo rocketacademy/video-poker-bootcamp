7ec1e09e746555eab29fb51b44df285d73c8efba
diyanarmln <diyana.rmln@gmail.com>
2021-09-20 20:13:45 +0800

    finished working game

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ const shuffleCards = (cards) => {
   return cards;
 };
 
-const deck = shuffleCards(makeDeck());
+let deck = shuffleCards(makeDeck());
 
 // ========== HELPER FUNCTIONS ==========
 /**
@@ -149,6 +149,16 @@ const xOfAKind = (x) => {
   return keys.length;
 };
 
+// function to check x of a kind and royal suit
+const xOfAKindRoyal = (x) => {
+  const keys = Object.keys(cardNameTally).filter((k) => cardNameTally[k] === x);
+  if (keys[0] === 'J' || keys[0] === 'Q' || keys[0] === 'K' || keys[0] === 'A') {
+    return keys.length;
+  }
+  return 0;
+  // console.log(keys);
+};
+
 // function to check suit tally
 const xOfSuit = (x) => {
   const keys = Object.keys(suitTally).filter((k) => suitTally[k] === x);
@@ -156,37 +166,130 @@ const xOfSuit = (x) => {
   return keys.length;
 };
 
+// function to display text in msg box
 const output = (msg) => {
   gameText.innerHTML = msg;
 };
 
+// hardcoded hand for testing
+const hardCodeHand = [
+  {
+    rank: 13, suit: 'hearts', displayName: 'K', suitSymbol: '♥️', keep: false,
+  },
+  {
+    rank: 12, suit: 'hearts', displayName: 'Q', suitSymbol: '♥️', keep: false,
+  },
+  {
+    rank: 11, suit: 'hearts', displayName: 'J', suitSymbol: '♥️', keep: false,
+  },
+  {
+    rank: 10, suit: 'hearts', displayName: '10', suitSymbol: '♥️', keep: false,
+  },
+  {
+    rank: 1, suit: 'hearts', displayName: 'A', suitSymbol: '♥️', keep: false,
+  },
+];
+
 let winValue = '';
 let straightTally = [];
+let isStraightRoyal = false;
 
+// function to check for straights in hand
 const checkStraights = () => {
+  // create empty array to use for checking straights
   straightTally = [];
+  // create default straight condition as true
   let isStraight = true;
+  // create default secondCheck condition as false to check for A cards in reverse for royal suits
+  let secondCheck = false;
+  // push hand ranks into straight tally array
   for (let i = 0; i < hand.length; i += 1) {
     const cardRank = hand[i].rank;
     straightTally.push(cardRank);
   }
+  // sort straights array in ascending order
   straightTally.sort((a, b) => a - b);
-  // if no As
-  for (let j = 1; j < straightTally.length; j += 1) {
-    if (isStraight === true && straightTally[j] === straightTally[(j - 1)] + 1) {
-      isStraight = true;
+  console.log(straightTally);
+  // if there is A or rank 1 hand in straight tally
+  if (straightTally[0] === 1) {
+    // loop to check each array value for straight condition
+    for (let j = 1; j < straightTally.length; j += 1) {
+      // check if current index is consecutive to the index before it
+      if (isStraight === true && straightTally[j] === straightTally[(j - 1)] + 1) {
+        // if consecutive, set straight condition to true
+        isStraight = true;
+        console.log(isStraight);
+      } else {
+        // if not consecutive, end the loop and set condition to false
+        console.log('reached');
+        j = straightTally.length + 1;
+        isStraight = false;
+        // turn on secondCheck condition so that we can check consecutive for A in reverse order
+        secondCheck = true;
+      }
+    }
+  } if (secondCheck === true) {
+    console.log('reached');
+    // reset straight condition to default value
+    isStraight = true;
+    console.log(straightTally);
+    // replace A card with rank 14
+    straightTally.splice(0, 1, 14);
+    console.log(straightTally);
+    // sort straight tally so that loop can check consecutive
+    straightTally.sort((a, b) => a - b);
+    console.log(straightTally);
+    // run loop to check if each index is consecutive to the one before
+    for (let j = 1; j < straightTally.length; j += 1) {
+      if (isStraight === true && straightTally[j] === straightTally[(j - 1)] + 1) {
+        // if condition met, set straight value to true
+        isStraight = true;
+        console.log(isStraight);
+      } else {
+        // end loop and set straight condition to false
+        j = straightTally.length + 1;
+        isStraight = false;
+      }
       console.log(isStraight);
-    } isStraight = false;
-    console.log(isStraight);
+    }
+    if (isStraight === true) {
+      isStraightRoyal = true;
+    }
   }
-  // if got A
+  else {
+    // if no As in straight tally, run loop to check for consecutive values
+    for (let k = 1; k < straightTally.length; k += 1) {
+      if (isStraight === true && straightTally[k] === straightTally[(k - 1)] + 1) {
+        // if condition met, set straight value to true
+        isStraight = true;
+        console.log(isStraight);
+      } else {
+        // if not met, end loop and set straight value to false
+        k = straightTally.length + 1;
+        isStraight = false;
+      }
+      console.log(isStraight);
+    }
+  }
+  // return straight value as true or false
   return isStraight;
 };
 
 const calcHandScore = (hand) => {
+  // populate tally object for card name in hand
   populCardNameTally();
+  // populate tally object for suit in hand
   populSuitTally();
-  // 1 five of a kind
+  // 1 royal flush (800)
+  if (xOfSuit(5) === 1 && checkStraights() === true && isStraightRoyal === true) {
+    winValue = 'royal flush';
+    if (playerBet === 5) {
+      console.log(playerBet);
+      return getResult(800);
+      console.log('reached');
+    } return getResult(250);
+    console.log('reached also');
+  }
   // 2 straight flush
   if (xOfSuit(5) === 1 && checkStraights() === true) {
     winValue = 'straight flush';
@@ -201,7 +304,7 @@ const calcHandScore = (hand) => {
   // 4 full house
   if (xOfAKind(3) === 1 && xOfAKind(2) === 1) {
     winValue = 'full house';
-    return getResult(10);
+    return getResult(9);
   }
   // 5 flush
   if (xOfSuit(5) === 1) {
@@ -211,38 +314,34 @@ const calcHandScore = (hand) => {
   // 6 straight
   if (checkStraights() === true) {
     winValue = 'straights';
-    return getResult(5);
+    return getResult(4);
   }
   // 7 three of a kind
   if (xOfAKind(3) === 1) {
     // winRank = 7;
     winValue = 'three of a kind';
-    return getResult(4);
+    return getResult(3);
   }
   // 8 two pair
   if (xOfAKind(2) === 2) {
     // winRank = 7;
     winValue = 'two pair';
-    return getResult(3);
-  }
-  // 9 one pair
-  if (xOfAKind(2) === 1) {
-    // winRank = 7;
-    winValue = 'one pair';
     return getResult(2);
   }
-  // 10 high card
-  if ('K' in cardNameTally || 'Q' in cardNameTally || 'J' in cardNameTally || 'A' in cardNameTally) {
-    winValue = 'high card';
-    return getResult(1);
+  // 9 one pair
+  // === TO CHANGE TO PAIR OF JACKS OR BETTER ===
+  if (xOfAKindRoyal(2) === 1) {
+    // winRank = 7;
+    winValue = 'jacks or better';
+    return getResult(2);
   }
   return forLosers();
 };
 
 const getResult = (multiplier) => {
   console.log(winValue);
-  const score = playerBet * multiplier;
-  output(`You won ${winValue} and ${score} points.<br>`);
+  const score = Number(playerBet) * multiplier;
+  output(`You won ${winValue}.<br>You bet ${playerBet} credit(s) and won ${score} points.<br>`);
   playerPoints += Number(score);
   creditNumber.innerHTML = `${playerPoints}`;
 };
@@ -328,20 +427,6 @@ const maxButton = document.createElement('button');
 maxButton.innerText = 'max';
 maxButton.addEventListener('click', (event) => { maxButtonClick(betAmount); });
 
-// create hold/unhold buttons for each card on board
-for (let i = 0; i < 5; i += 1) {
-  const holdButton = document.createElement('button');
-  holdButton.classList.add('keep');
-  holdButton.innerText = 'Keep';
-  holdButton.addEventListener('click', (event) => {
-    // we will want to pass in the card element so
-    // that we can change how it looks on screen, i.e.,
-    // "turn the card over"
-    holdButtonClickEvent(event.currentTarget, i);
-    console.log(event.currentTarget);
-  });
-  buttonRow.appendChild(holdButton);
-}
 let hand = [];
 
 // function to display dealt cards
@@ -362,19 +447,49 @@ const displayCards = () => {
     cardElement.classList.add('card');
     boardCard.appendChild(cardElement);
   }
+
+  // create hold/unhold buttons for each card on board
+
+  buttonRow.innerHTML = '';
+
+  for (let i = 0; i < 5; i += 1) {
+    const holdButton = document.createElement('button');
+    holdButton.classList.add('keep');
+    holdButton.innerText = 'Keep';
+    holdButton.addEventListener('click', (event) => {
+    // we will want to pass in the card element so
+    // that we can change how it looks on screen, i.e.,
+    // "turn the card over"
+      holdButtonClickEvent(event.currentTarget, i);
+      console.log(event.currentTarget);
+    });
+    buttonRow.appendChild(holdButton);
+  }
+};
+
+const checkDeck = () => {
+  if (deck.length < 5) {
+    deck = [];
+    deck = shuffleCards(makeDeck());
+    console.log('NEWDECK');
+  }
 };
 
 // create deal click event to display dealt cards
 const dealButtonClickEvent = () => {
-  if (playerBet > 0) {
+  // check playerBet in case bet amount entered manually
+  playerBet = Number(betAmount.value);
+  if (playerBet > 0 && playerPoints > 0) {
     if (gameMode === 'default') {
       cardNameTally = {};
       suitTally = {};
       straightTally = {};
+      checkDeck();
       for (let i = 0; i < 5; i += 1) {
         const card = deck.pop();
         hand.push(card);
       }
+      // hand = hardCodeHand;
       displayCards();
       board.appendChild(buttonRow);
       gameMode = 'secondDeal';
@@ -384,9 +499,29 @@ const dealButtonClickEvent = () => {
       calcHandScore();
       gameMode = 'default';
       hand = [];
-      gameText.innerHTML += 'Click \'Deal\' to play another round.';
+      gameText.innerHTML += 'Enter bet and click \'Deal\' to play another round.';
+      betAmount.value = 0;
+      playerBet = Number(0);
     }
-  } else { gameText.innerHTML = 'Please enter your bet'; }
+  }
+  else if (playerPoints <= 0) {
+    output('You have no more credits. Game Over.<br>Click "Reset" to start new game');
+  }
+  else { gameText.innerHTML = 'Please enter your bet'; }
+};
+
+const resetButtonClickEvent = () => {
+  deck = [];
+  deck = shuffleCards(makeDeck());
+  playerPoints = 100;
+  creditNumber.innerHTML = `${playerPoints}`;
+  playerBet = Number(0);
+  betAmount.value = `${playerBet}`;
+  hand = [];
+  gameMode = 'default';
+  boardCard.innerHTML = '';
+  buttonRow.innerHTML = '';
+  gameText.innerHTML = 'Enter bet and click \'Deal\' to begin the poker game';
 };
 
 // create deal button
@@ -395,6 +530,13 @@ dealButton.classList.add('button');
 dealButton.innerText = 'Deal / Draw';
 dealButton.addEventListener('click', dealButtonClickEvent);
 buttonRow2.appendChild(dealButton);
+
+// create reset button
+const resetButton = document.createElement('button');
+resetButton.classList.add('button');
+resetButton.innerText = 'Reset';
+resetButton.addEventListener('click', resetButtonClickEvent);
+buttonRow2.appendChild(resetButton);
 
 // append buttonrow3 elements
 buttonGroupCredit.appendChild(creditNumber);
@@ -416,13 +558,16 @@ const holdButtonClickEvent = (cardElement, index) => {
       hand[index].keep = true;
     } else { hand[index].keep = false; }
   }
-  console.log(index, hand[index], hand[index].keep);
+  else {
+    output('Round over.<br>Enter bet and click "Deal" to play again.');
+  }
 };
 
 // create function to deal released cards , calculate hand score, and update points
 // eslint-disable-next-line max-len
 // when condition is true, use splice to remove that index and replace with new card splice(index, 1, deck.pop)
 const reDeal = () => {
+  checkDeck();
   for (let i = 0; i < hand.length; i += 1) {
     if (hand[i].keep === false) {
       hand.splice(i, 1, deck.pop());
@@ -435,24 +580,56 @@ const reDeal = () => {
 // function for plus minus buttons
 const plusButtonClick = (target) => {
   const currentValue = Number(target.value);
-  if (currentValue < playerPoints) {
-    target.value = currentValue + 1; }
-  playerBet = target.value;
+  if (gameMode === 'default') {
+    if (currentValue < 5) {
+      target.value = currentValue + 1; }
+    playerBet = target.value;
+  } else {
+    output('Game in progress.<br>Bet cannot be changed at this time.');
+  }
 };
 
 const minusButtonClick = (target) => {
   const currentValue = Number(target.value);
-  if (currentValue >= 1) {
-    target.value = currentValue - 1;
+  if (gameMode === 'default') {
+    if (currentValue >= 1) {
+      target.value = currentValue - 1;
+    } else {
+      playerBet = Number(target.value);
+    }
+  } else {
+    output('Game in progress.<br>Bet cannot be changed at this time.');
   }
-  playerBet = target.value;
 };
 
 const maxButtonClick = (target) => {
-  target.value = playerPoints;
-  playerBet = target.value;
+  if (gameMode === 'default') {
+    target.value = 5;
+    playerBet = Number(target.value);
+  } else {
+    output('Game in progress.<br>Bet cannot be changed at this time.');
+  }
 };
 
-// add check for finished deck
+// update winning points for max bet for royal flush --- done
+// update winning conditions - royal flush --- done
+// update winning conditions - jacks or better --- done
 
-// duplicate cards in hand giving issues in updating object keys and splicing hand array
+// organise code
+// jsdoc
+// if points run out, pop up window buy credits
+// clicked card is highlighted -- no keep buttons
+// add click delay when bet buttons are clicked to revert back to initial game text
+
+// css improvements
+// sound on/off?
+// transition effects when cards are dealt
+// start game with delay for shuffling etc and animations
+// celebration animation when won with highlight on which pay schedule won and how many credits
+// clicked card is highlighted -- no keep buttons
+// maybe push bet amount and credit to the top? buttons below.
+// modern attractive colors
+// instead of reset, dom element pops up when playerPoints is 0. get user to buy more coins
+// ability to hide/show pay schedule
+// player name
+// leaderboard
