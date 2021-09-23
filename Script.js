845ae30f4d04c52ed1d788b0c007a4a990f65f0a
tanfeng95 <31460702+tanfeng95@users.noreply.github.com>
2021-09-23 20:45:00 +0800

    update on css/documenation/bug fix

--- Script.js
+++ Script.js
@@ -120,11 +120,13 @@ const createCard = (cardInfo) => {
   return card;
 };
 
+// array for the cards in the 5 cards being poo
 let cardArray = [];
 
 // function that deal 5 cards when the user press deal and auto reduce a single point
 // return NIL
 const dealFiveCards = () => {
+  dealBtn.disabled = true;
   // make the comment div  string empty
   commentDiv.innerText = '';
   // reduce 1 point after pressing deal button  and update the points
@@ -161,6 +163,8 @@ const dealFiveCards = () => {
     // append the card element to the div
     cardDiv.appendChild(cardElement);
   });
+  commentDiv.innerText = 'Please click the card you want to change and press the change button after selection ';
+  changeBtn.disabled = false;
 };
 
 let selectedCardsElementArray = [];
@@ -195,7 +199,10 @@ const cardClicked = (card, cardElement) => {
 // return NIL
 const change = () => {
   // remove the selected card from the orginal array
+  // 2 spade if it inside the  selectedCardArray true equal it will return
+  // true turn into false and deleted away from the card array
   cardArray = cardArray.filter((val) => !selectedCardArray.includes(val));
+
   console.log(cardArray);
 
   for (let i = 0; i < selectedCardsElementArray.length; i++) {
@@ -214,8 +221,13 @@ const change = () => {
   selectedCardsArray = [];
   selectedCardsElementArray = [];
   calcHandScore();
+  changeBtn.disabled = true;
+  resetBtn.disabled = false;
+  commentDiv.innerText = 'Current hand completed please press next game to continue playing';
+  cardDiv.classList.add('disabledCards');
 };
 
+// test hand  for testing of ans
 const playerHand = [
   { rank: 1, suit: 'spades', name: '2' },
   { rank: 2, suit: 'spades', name: '2' },
@@ -239,26 +251,20 @@ let isStraightTrue = false;
 let isStraightFlush = false;
 let isFullHouseTrue = false;
 
+// a method to  calulate handscore
+// return NIL
 const calcHandScore = () => {
-  // reset everything before starting
+  // reset all the arrays before calulating
   cardYouGot.innerText = '';
   suitArray = [];
   rankArray = [];
   cardNameTally = [];
   // Loop over hand
-
-  // points -= 1;
-  // updatePoint(points);
   console.log(cardArray.length);
   for (let i = 0; i < cardArray.length; i += 1) {
     const cardName = cardArray[i].rank;
-    // const cardName = playerHand[i].rank;
-
     rankArray.push(cardName);
-
-    // suitArray.push(playerHand[i].suit);
     suitArray.push(cardArray[i].suit);
-
     // If we have seen the card name before, increment its count
     if (cardName in cardNameTally) {
       cardNameTally[cardName] += 1;
@@ -280,14 +286,13 @@ const calcHandScore = () => {
     // 1 pair
     if (cardNameTally[cardName] === 2) {
       // console.log(`${cardName} rank 1 pair`);
-      cardYouGot.innerText += ` ${cardName} rank card 1 pair, `;
       isOnePairTrue = true;
     }
 
     // 3 of a kind
     if (cardNameTally[cardName] === 3) {
       // console.log(`${cardName} rank has 3 of a kind`);
-      cardYouGot.innerText += ` ${cardName} rank card has 3 of a kind, `;
+
       isThreeOfAKindTrue = true;
       // console.log(isThreePairTrue);
     }
@@ -295,36 +300,25 @@ const calcHandScore = () => {
     // 4 of a kind
     if (cardNameTally[cardName] === 4) {
       // console.log(`${cardName} rank has 4 of a kind`);
-      cardYouGot.innerText += ` ${cardName} rank card has 4 of a kind, `;
       isFourOfAKindTrue = true;
     }
     // 5 of a kind
     if (cardNameTally[cardName] === 5) {
       // console.log(`${cardName} rank has 5 of a kind`);
-      cardYouGot.innerText += ` ${cardName} rank card has 4 of a kind, `;
       isFiveOfAKindTrue = true;
     }
   }
   // high card
-  const afterFilterLength = rankArray.filter((v, i, a) => a.indexOf(v) === i);
-  console.log(` the length is ${afterFilterLength.length}`);
-  if (afterFilterLength.length === 5) {
-    IsHighCardTrue = true;
-    console.log(`hightcard = ${highcard} `);
-    cardYouGot.innerText += ` hightcard = ${highcard}, `;
-  }
+  highCardNum();
   // 2 pair
   const findOutTwoPair = twoPair(rankArray);
   if (findOutTwoPair) {
     isTwoPairTrue = true;
     // console.log('you have 2 pairs in your hand ');
-    cardYouGot.innerText += ' you have 2 pairs in your hand, ';
   }
   // flush
   console.log(suitArray);
   if (sameSuit(suitArray)) {
-    // console.log('you have a same suit in your hand ');
-    cardYouGot.innerText += 'you have a same suit in your hand, ';
     isFlushTrue = true;
   }
 
@@ -340,73 +334,103 @@ const calcHandScore = () => {
   }
 
   if (straightResultCount === 4) {
-    // console.log('you have a straights  ');
     isStraightTrue = true;
-    cardYouGot.innerText += 'you have a straights,  ';
   }
 
   // straight flush
   if (sameSuit(suitArray) === true && straightResultCount === 4) {
-    // console.log('you have a straights flush  ');
     isStraightFlush = true;
-    cardYouGot.innerText += 'you have a straights flush,  ';
   }
 
   // full house
   if (fullHouse()) {
-    // console.log('you have a full house');
     isFullHouseTrue = true;
-    cardYouGot.innerText += 'you have a full house,  ';
   }
   giveScore();
+  updatePoint(points);
 };
 
+// method to give score and update the points array
+// return NIL
 const giveScore = () => {
   if (isFiveOfAKindTrue)
   {
     points += 10;
+    cardYouGot.innerText += ' You have 5 of a kind, ';
     cardYouGot.innerText += ' + 10 points';
+    return;
   }
-  else if (isStraightFlush) {
+  if (isStraightFlush) {
     points += 9;
+    cardYouGot.innerText += 'you have a straights flush,  ';
     cardYouGot.innerText += ' + 9 points';
+    return;
   }
-  else if (isFourOfAKindTrue) {
+  if (isFourOfAKindTrue) {
     points += 8;
+    cardYouGot.innerText += ' You have 4 of a kind, ';
     cardYouGot.innerText += ' + 8 points';
+    return;
   }
-  else if (isFullHouseTrue) {
+  if (isFullHouseTrue) {
     points += 7;
+    cardYouGot.innerText += 'you have a full house,  ';
     cardYouGot.innerText += ' + 7 points';
+    return;
   }
-  else if (isFlushTrue) {
+  if (isFlushTrue) {
     points += 6;
+    cardYouGot.innerText += 'you have a flush, ';
     cardYouGot.innerText += ' + 6 points';
+    return;
   }
-  else if (isStraightTrue) {
+  if (isStraightTrue) {
     points += 5;
+    cardYouGot.innerText += 'you have a straights,  ';
     cardYouGot.innerText += ' + 5 points';
+    return;
   }
-  else if (isThreeOfAKindTrue) {
+  if (isThreeOfAKindTrue) {
     points += 4;
+    cardYouGot.innerText += ' You have 3 of a kind, ';
     cardYouGot.innerText += ' + 4 points';
+    return;
   }
-  else if (isTwoPairTrue) {
+  if (isTwoPairTrue) {
     points += 3;
+    cardYouGot.innerText += ' You have 2 pair , ';
     cardYouGot.innerText += ' + 3 points';
+    return;
   }
-  else if (isOnePairTrue) {
+  if (isOnePairTrue) {
     points += 2;
+    cardYouGot.innerText += ' You have 1 pair , ';
     cardYouGot.innerText += ' + 2 points';
-  } else if (IsHighCardTrue) {
+    return;
+  } if (IsHighCardTrue) {
     points += 0;
-    cardYouGot.innerText += ' + NIL';
+    cardYouGot.innerText += ' A Hightcard , ';
+    cardYouGot.innerText += ' + NIL points';
   }
-
-  updatePoint(points);
 };
 
-// full house
+// high card number
+
+// method highCardNum filter the rank array and check if there any repeat numbers and filter it out
+// if the lenth is still 5 , there is a high cards
+// return NIL
+const highCardNum = () => {
+  // indexof returns the first index at which a given element can be found in the array,
+  // filter return true = keep false = delete
+  const afterFilterLength = rankArray.filter((element, index, array) => array.indexOf(element) === index);
+  console.log(` the length is ${afterFilterLength.length}`);
+  if (afterFilterLength.length === 5) {
+    IsHighCardTrue = true;
+  }
+};
+
+// full house method if there 3 of a kind and one pair return true else false
+// return Bool
 const fullHouse = () => {
   // console.log(`  three pair = ${isThreeOfAKindTrue}`);
   if (isOnePairTrue && isThreeOfAKindTrue) {
@@ -417,12 +441,16 @@ const fullHouse = () => {
 
 // straight
 let straightCount = [];
+// straightCards method that count if the array is increment by 1
+// arg1 = array[int]
+// return array[bool]
 const straightCards = (arr) => {
   arr.sort((a, b) => {
     if (a > b) return 1;
     if (a < b) return -1;
     return 0;
   });
+  // [5 6 6 8 9] [ 6 6 8 9 ]
   console.log(arr);
   const nextArr = arr.slice(1);
   for (let i = 0; i < nextArr.length; i++) {
@@ -438,11 +466,14 @@ const straightCards = (arr) => {
   return straightCount;
 };
 
-// flush method
+// flush method find out if the 5 cards have the same suit
+// arg1 = array[string]
+// return bool
 let suitCount = {};
 let result = 0;
 let suitResult = false;
 const sameSuit = (arr) => {
+  suitResult = false;
   for (let i = 0; i < arr.length; i += 1) {
     const suitName = arr[i];
     // If we have seen the card name before, increment its count
@@ -466,21 +497,31 @@ const sameSuit = (arr) => {
 };
 
 // find out if it 2 pair
+// need to explain from documention
+// twopair find out if there 2 pair in the 5 cards
+// arg1 = array[int]
+// return bool
 const twoPair = (arr) => {
   const countObj = {};
   for (const x of arr) {
     countObj[x] = (countObj[x] || 0) + 1;
   }
+  // change it into a array
   const vals = Object.values(countObj);
   // console.log(vals);
+  // if there a card have 2 pairs and the numbers of cards = 2 return true;
   if (vals.filter((x) => x === 2).length == 2) return true;
   return false;
 };
-
+// method to update the points
+// arg1 = int
+// return NIL
 const updatePoint = (updatedPoints) => {
   totalPoints.innerHTML = updatedPoints;
 };
 
+// method to reset all cards and array
+// return NIL
 const reset = () => {
   console.log('reset');
 
@@ -506,22 +547,38 @@ const reset = () => {
   isStraightTrue = false;
   isStraightFlush = false;
   isFullHouseTrue = false;
+  highcard = 0;
+  suitResult = false;
+
+  resetBtn.disabled = true;
+  dealBtn.disabled = false;
+  commentDiv.innerHTML = 'Please press deal to start the game';
+  cardDiv.classList.remove('disabledCards');
 };
 
+let gamediv;
 let buttonDiv;
 let cardDiv;
 let commentDiv;
 let totalPoints;
 let cardYouGot;
-// total point be 100
 let points = 100;
+let changeBtn;
+let dealBtn;
+let resetBtn;
 
+// method to construct the page
+// return NIL
 const init = () => {
+  gamediv = document.createElement('div');
+  gamediv.classList.add('gamediv');
+
   const scoreDiv = document.createElement('div');
   scoreDiv.classList.add('scoreDiv');
   const score = document.createElement('h4');
-  score.innerHTML = 'Score : ';
+  score.innerHTML = 'Current Score : ';
   totalPoints = document.createElement('h4');
+  totalPoints.classList.add('scoreDiv');
   totalPoints.innerHTML = points;
 
   const textContentDiv = document.createElement('div');
@@ -529,9 +586,10 @@ const init = () => {
   const textContent = document.createElement('h4');
   textContent.innerHTML = ' You Got : ';
   cardYouGot = document.createElement('h4');
+  cardYouGot.classList.add('textContentDiv');
 
   buttonDiv = document.createElement('div');
-  const dealBtn = document.createElement('button');
+  dealBtn = document.createElement('button');
   dealBtn.textContent = 'Deal';
   dealBtn.addEventListener('click', dealFiveCards);
 
@@ -539,8 +597,9 @@ const init = () => {
   // testBtn.textContent = 'test';
   // testBtn.addEventListener('click', calcHandScore);
 
-  const resetBtn = document.createElement('button');
-  resetBtn.textContent = 'reset';
+  resetBtn = document.createElement('button');
+  resetBtn.textContent = 'Next Game';
+  resetBtn.disabled = true;
   resetBtn.addEventListener('click', reset);
 
   cardDiv = document.createElement('div');
@@ -548,24 +607,28 @@ const init = () => {
   cardDiv.setAttribute('id', 'cardDiv');
 
   commentDiv = document.createElement('div');
+  commentDiv.classList.add('commentDiv');
   commentDiv.innerHTML = 'Please press deal to start the game';
   commentDiv.style.color = 'red';
 
-  const changeBtn = document.createElement('button');
+  changeBtn = document.createElement('button');
   changeBtn.textContent = 'change';
+  changeBtn.disabled = true;
   changeBtn.addEventListener('click', change);
 
-  document.body.appendChild(scoreDiv).append(score);
-  document.body.appendChild(scoreDiv).append(totalPoints);
-  document.body.appendChild(textContentDiv).append(textContent);
-  document.body.appendChild(textContentDiv).append(cardYouGot);
+  document.body.appendChild(gamediv);
 
-  document.body.appendChild(commentDiv);
-  document.body.appendChild(buttonDiv).append(dealBtn);
-  document.body.appendChild(buttonDiv).append(changeBtn);
+  gamediv.appendChild(scoreDiv).append(score);
+  gamediv.appendChild(scoreDiv).append(totalPoints);
+  gamediv.appendChild(textContentDiv).append(textContent);
+  gamediv.appendChild(textContentDiv).append(cardYouGot);
+
+  gamediv.appendChild(commentDiv);
+  gamediv.appendChild(buttonDiv).append(dealBtn);
+  gamediv.appendChild(buttonDiv).append(changeBtn);
   // document.body.appendChild(buttonDiv).append(testBtn);
-  document.body.appendChild(buttonDiv).append(resetBtn);
-  document.body.appendChild(cardDiv);
+  gamediv.appendChild(buttonDiv).append(resetBtn);
+  gamediv.appendChild(cardDiv);
 };
 
 init();
