b21d5201d36c2299904e637568addd97ac709f7e
Nuuggs <69680070+Nuuggs@users.noreply.github.com>
2021-10-14 22:51:39 +0800

    base logic working

--- script.js
+++ script.js
@@ -22,7 +22,9 @@
 const playerPoints = 100;
 const pointsBet = 0;
 
-const gameDeck = '';
+let gameDeck = '';
+const playerHand = [];
+let cardElement;
 
 // ----- html ----- //
 const messageInterface = document.createElement('div');
@@ -137,10 +139,156 @@ const shuffleCards = (cards) => {
   return cards;
 };
 
+const getGameDeck = () => {
+  gameDeck = shuffleCards(createDeck());
+  console.log(gameDeck);
+  return gameDeck;
+};
+
+const drawHand = () => {
+  for (let i = 0; i < 5; i += 1) {
+    // create virtual hand
+    const card = gameDeck.pop();
+    playerHand.push(card);
+
+    // create UI hand
+  }
+  console.log(playerHand[0]);
+  console.log(playerHand[1]);
+  console.log(playerHand[2]);
+  console.log(playerHand[3]);
+  console.log(playerHand[4]);
+
+  return playerHand;
+};
+
+const sortHand = (inputArray) => {
+  let sorted = false;
+  while (sorted === false) {
+    let swapCounter = 0;
+    for (let i = 0; i < inputArray.length - 1; i += 1) {
+      if (inputArray[i].rank > inputArray[i + 1].rank) {
+        const tempObj = inputArray[i + 1];
+        inputArray[i + 1] = inputArray[i];
+        inputArray[i] = tempObj;
+        swapCounter += 1;
+      } else if (i === inputArray.length - 2 && swapCounter === 0) {
+        sorted = true;
+      }
+    }
+  }
+  return inputArray;
+};
+const createCard = (cardInfo) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardName = document.createElement('div');
+  cardName.classList.add(cardInfo.name, cardInfo.color);
+  cardName.innerText = cardInfo.displayName;
+
+  const cardSuit = document.createElement('div');
+  cardSuit.classList.add(cardInfo.suit, cardInfo.color);
+  cardSuit.innerText = cardInfo.symbol;
+
+  card.appendChild(cardName);
+  card.appendChild(cardSuit);
+
+  return card;
+};
+
+const instantiateHand = (handArray) => {
+  for (let i = 0; i < handArray.length; i += 1) {
+    cardElement = createCard(handArray[i]);
+    gameInterface.appendChild(cardElement);
+  }
+};
+
+const checkHandCondition = (handArray) => {
+  let handCondition;
+
+  // cheat hand
+  // const handArray = [
+  //   { name: 'ace', suit: 'hearts', rank: 1 },
+  // { name: 'ace', suit: 'diamonds', rank: 1 },
+  // { name: 'ace', suit: 'spades', rank: 1 },
+  // { name: 'ace', suit: 'clubs', rank: 5 },
+  // { name: '5', suit: 'hearts', rank: 5 },
+  // ];
+
+  // condition checkers
+  const matchArray = [];
+  let straightCond = true;
+  let flushCond = true;
+
+  // loop through entire hand
+  for (let i = 0; i < handArray.length - 1; i += 1) {
+    // if find a match, push to matchArray
+    if (handArray[i].rank === handArray[i + 1].rank) {
+      matchArray.push(handArray[i].rank);
+    }
+    // check for flush
+    if (handArray[i].suit !== handArray[i + 1].suit) {
+      flushCond = false;
+    }
+    // check for straight
+    if (handArray[i].rank !== handArray[i + 1].rank - 1) {
+      straightCond = false;
+    }
+    console.log(handArray[i]);
+  }
+
+  // CHEATS
+  // flushCond = true;
+  // straightCond = true;
+
+  console.log(`Match Array: ${matchArray}`);
+  console.log(`Match Array Length: ${matchArray.length}`);
+  console.log(handArray[0].rank);
+  console.log(handArray[4].rank);
+
+  console.log(`straight cond: ${straightCond}, flush cond: ${flushCond}`);
+
+  // determine hand conditions
+
+  if (matchArray.length === 0 && straightCond === true && flushCond === true) {
+    handCondition = 9; // STRAIGHT FLUSH
+  } else if (matchArray.length === 3
+    && matchArray[0] === matchArray[1] && matchArray[0] === matchArray[2]) {
+    handCondition = 8; // FOUR OF A KIND
+  } else if (matchArray.length === 3
+    && (matchArray[0] !== matchArray[1] || matchArray[0] !== matchArray[2])) {
+    handCondition = 7; // FULL HOUSE
+  } else if (matchArray.length === 0 && flushCond === true && straightCond === false) {
+    handCondition = 6; // FLUSH
+  } else if (matchArray.length === 0 && straightCond === true && flushCond === false) {
+    handCondition = 5; // STRAIGHT
+  } else if (matchArray.length === 2 && matchArray[0] === matchArray[1]) {
+    handCondition = 4; // THREE OF A KIND
+  } else if (matchArray.length === 2 && matchArray[0] !== matchArray[1]) {
+    handCondition = 3; // TWO PAIR
+  } else if (matchArray.length === 1) {
+    handCondition = 2; // ONE PAIR
+  } else if (matchArray.length === 0 && (handArray[0].rank === 1 || handArray[4].rank > 10)) {
+    handCondition = 1; // HIGH CARD
+  } else {
+    handCondition = 0; // LOSE
+  }
+  console.log(`Hand Condition: ${handCondition}`);
+  return handCondition;
+};
 // ========== INITIALISATION ========== //
 const initGame = () => {
   document.body.appendChild(messageInterface);
   document.body.appendChild(gameInterface);
+
+  getGameDeck();
+  // sort hand in ascending order of rank
+  const firstHand = sortHand(drawHand());
+  // create hand based on card data
+  instantiateHand(firstHand);
+
+  checkHandCondition(firstHand);
 };
 
 initGame();
