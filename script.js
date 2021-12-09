109ae5f76c679fe79b3bc6ae32af226ff01c1790
Aqif Sariman <aqifsariman@users.noreply.github.com>
2021-12-09 16:56:25 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,87 @@
+/* 
+* A function that returns a fixed number of points
+* @param cardInHand = [] to store all cards drawn
+* @param push cards drawn into cardInHand
+* @return number of points that the user scored for the cards in hand
+ */
+ 
+let cardInHand =[];
+let userPoints = 0;
+
+// buttons creation
+ let dealButton = document.createElement("button")
+ dealButton.id = "deal-button"
+ dealButton.innerHTML = "DEAL"
+ document.body.appendChild(dealButton)
+
+
+const makeDeck = (cardAmount) => {
+  // create the empty deck at the beginning
+  const newDeck = [];
+  const suits = ["❤", "♦", "♣", "♠"];
+
+  for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    // make a variable of the current suit
+    const currentSuit = suits[suitIndex];
+    /* console.log(`current suit: ${currentSuit}`); */
+
+    // loop to create all cards in this suit
+    // rank 1-13
+    for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // Convert rankCounter to string
+      let cardName = `${rankCounter}`;
+
+      // 1, 11, 12 ,13
+      if (cardName === "1") {
+        cardName = "A";
+      } else if (cardName === "11") {
+        cardName = "J";
+      } else if (cardName === "12") {
+        cardName = "Q";
+      } else if (cardName === "13") {
+        cardName = "K";
+      }
+      if (currentSuit === "❤" || currentSuit === "♦") {
+        var color = "red";
+      } else if (currentSuit === "♣" || currentSuit === "♠") {
+        var color = "black";
+      }
+
+      // make a single card object variable
+      const card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+        cardColor: color,
+      };
+
+      // add the card to the deck
+      newDeck.push(card);
+    }
+}
+}
+
+const getRandomIndex = (max) => Math.floor(Math.random() * max);
+
+const shuffleCards = (cards) => {
+  // Loop over the card deck array once
+  for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
+    // Select a random index in the deck
+    const randomIndex = getRandomIndex(cards.length);
+    // Select the card that corresponds to randomIndex
+    const randomCard = cards[randomIndex];
+    // Select the card that corresponds to currentIndex
+    const currentCard = cards[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cards[currentIndex] = randomCard;
+    cards[randomIndex] = currentCard;
+  }
+  // Return the shuffled deck
+  return cards;
+}
+
+let shuffledCards = shuffleCards(newDeck)
+
+const calcHandScore = (inHand) => {
+	let drawCards = shuffledCards.pop();
+};
