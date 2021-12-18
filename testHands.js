f86949faa7426a5a2e2b0e570391705cf86f418d
kennethongcs <kenneth.ongcs@gmail.com>
2021-12-18 18:30:36 +0800

    modified testHands

--- testHands.js
+++ testHands.js
@@ -1,9 +1,9 @@
 const playerHand = [
   { rank: 2, suit: 'hearts', name: '2' },
-  { rank: 1, suit: 'hearts', name: '2' },
   { rank: 3, suit: 'hearts', name: '2' },
-  { rank: 4, suit: 'hearts', name: '2' },
+  { rank: 2, suit: 'hearts', name: '2' },
   { rank: 5, suit: 'hearts', name: '2' },
+  { rank: 4, suit: 'hearts', name: '2' },
 ];
 
 // calcHandScore returns the number of points a given hand earns.
