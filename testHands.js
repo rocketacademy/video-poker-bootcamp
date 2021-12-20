fcc0154a09429f6c72097f2920880c5552ed72ea
kennethongcs <kenneth.ongcs@gmail.com>
2021-12-20 21:27:23 +0800

    add royal flush to testing

--- testHands.js
+++ testHands.js
@@ -1,9 +1,9 @@
 const playerHand = [
-  { rank: 5, suit: 'hearts', name: '2' },
-  { rank: 4, suit: 'hearts', name: '2' },
-  { rank: 2, suit: 'hearts', name: '2' },
-  { rank: 2, suit: 'hearts', name: '2' },
-  { rank: 2, suit: 'hearts', name: '2' },
+  { rank: 13, suit: 'hearts', name: '2' },
+  { rank: 12, suit: 'hearts', name: '2' },
+  { rank: 11, suit: 'hearts', name: '2' },
+  { rank: 10, suit: 'hearts', name: '2' },
+  { rank: 1, suit: 'hearts', name: '2' },
 ];
 
 // calcHandScore returns the number of points a given hand earns.
