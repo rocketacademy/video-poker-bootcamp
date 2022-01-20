f2cd62022c07c9655a6d217911bb0ebbba435a27
liztanyl <elizabeth.tanyulin@gmail.com>
2022-01-20 16:46:31 +0800

    complete game, fix winning logic, added some styles

--- script.js
+++ script.js
@@ -14,13 +14,20 @@ let points = 100;
 const hand = [];
 const heldCardsIndex = [];
 let deck;
+let bet = 1;
+
+const cardNameTally = {};
+const cardSuitTally = {};
 
 // For HTML elements
 const mainContainer = document.createElement('div');
-const gameInstructions = document.createElement('div');
+const messageBoard = document.createElement('div');
 const pointsContainer = document.createElement('div');
 const cardContainer = document.createElement('div');
 const dealBtn = document.createElement('button');
+const gameInfoContainer = document.createElement('div');
+const gameInstructions = document.createElement('div');
+const payTable = document.createElement('div');
 
 // ***** HELPER FUNCTIONS *********************************
 // Functions for the deck-----------------------------
@@ -95,6 +102,31 @@ const shuffleCards = (cards) => {
 };
 // ---------------------------------------------------
 
+// Function to create empty card elements
+const createEmptyCardElements = () => {
+	cardContainer.innerHTML = ``;
+	for (let i = 0; i < 5; i += 1) {
+		// Create a wrapping div to contain the "hold" message and card
+		const cardWrapper = document.createElement('div');
+		cardWrapper.classList.add('card-div');
+
+		// Create a div for "hold" message
+		const holdMsg = document.createElement('div');
+		holdMsg.classList.add('hold');
+		holdMsg.innerHTML = '';
+
+		// Create a div for the card itself
+		const card = document.createElement('div');
+		card.classList.add('card', 'down');
+		card.innerHTML = `&nbsp;`;
+
+		// Append divs to cardWrapper and cardContainer
+		cardWrapper.appendChild(holdMsg);
+		cardWrapper.appendChild(card);
+		cardContainer.appendChild(cardWrapper);
+	}
+};
+
 // Function to create card elements
 const createCardElement = () => {
 	cardContainer.innerHTML = ``;
@@ -109,13 +141,21 @@ const createCardElement = () => {
 		const holdMsg = document.createElement('div');
 		holdMsg.classList.add('hold');
 		holdMsg.innerHTML = '';
+		if (heldCardsIndex.includes(i)) {
+			holdMsg.innerHTML = 'Hold';
+		}
 
 		// Create a div for the card itself
 		const card = document.createElement('div');
 		card.classList.add('card');
+		if (currentCard.suit === 'diamonds' || currentCard.suit === 'hearts') {
+			card.classList.add('red');
+		} else {
+			card.classList.add('black');
+		}
 		card.innerHTML = `&nbsp;`;
 		if (currentCard !== '') {
-			card.innerHTML = `${currentCard.name} ${currentCard.symbol}`;
+			card.innerHTML = `${currentCard.name} <span class="symbol">${currentCard.symbol}</span>`;
 		}
 
 		// Append divs to cardWrapper and cardContainer
@@ -125,16 +165,42 @@ const createCardElement = () => {
 
 		// Add event listener to each card
 		cardWrapper.addEventListener('click', () => {
-			// Add "hold" message
-			holdMsg.innerHTML = 'Hold';
-			// Store index of held cards in array
-			heldCardsIndex.push(i);
+			// // Add "hold" message
+			// holdMsg.innerHTML = 'Hold';
+			// // Store index of held cards in array
+			// heldCardsIndex.push(i);
+			toggleHold(holdMsg, i);
 		});
 	}
 };
 
-// Function to deal cards
-// Deal until hand.length = 5
+// Function to allow players to hold / un-hold a card
+const toggleHold = (msg, currentCardIndex) => {
+	if (msg.innerHTML === '') {
+		// Add "hold" message
+		msg.innerHTML = 'Hold';
+		// Store index of held cards in array
+		heldCardsIndex.push(currentCardIndex);
+	} else {
+		// Remove "hold" message
+		msg.innerHTML = '';
+		// Remove index of held cards in array
+		const indexToRemove = heldCardsIndex.indexOf(currentCardIndex);
+		heldCardsIndex.splice(indexToRemove, 1);
+	}
+};
+
+// Function to update points in
+const updatePoints = () => {
+	pointsContainer.innerText = `CREDIT ${points}`;
+};
+
+// Function to update message board
+const updateMessage = (message) => {
+	messageBoard.innerText = message;
+};
+
+// Function to deal cards & play the game
 const dealCards = () => {
 	// If it's the first time dealing
 	if (hand.length === 0) {
@@ -144,9 +210,9 @@ const dealCards = () => {
 		}
 		// Display cards
 		createCardElement();
-		mainContainer.appendChild(cardContainer);
 		dealBtn.innerText = `Draw`;
 	}
+
 	// If user selects cards to hold
 	else {
 		// Loop through hand array
@@ -158,26 +224,25 @@ const dealCards = () => {
 		}
 		createCardElement();
 
-		// Check hand against pay table
-		checkForWin(1); // remember to remove params later
+		checkForWin();
 
-		// Show message win or lose
-		// Update points
-		// setTimeout to remove cards and restart game
+		dealBtn.disabled = true;
+		setTimeout(newGame, 3000);
 
 		dealBtn.innerText = `Deal`;
 	}
 };
 
-// need a function to check hand against pay table
-// const checkForWin = () => {}
-
-// ***** GAMEPLAY *****************************************
-
-// To check for win
-// remember to remove params later
-const checkForWin = (bet) => {
-	const cardNameTally = {};
+// Function to tally cards
+const tallyCards = () => {
+	// Clear card tally
+	for (cardName in cardNameTally) {
+		delete cardNameTally[cardName];
+	}
+	for (cardSuit in cardSuitTally) {
+		delete cardSuitTally[cardSuit];
+	}
+	// Tally card name
 	for (let i = 0; i < hand.length; i += 1) {
 		const cardName = hand[i].name;
 		if (cardName in cardNameTally) {
@@ -186,9 +251,7 @@ const checkForWin = (bet) => {
 			cardNameTally[cardName] = 1;
 		}
 	}
-	console.log(cardNameTally);
-
-	const cardSuitTally = {};
+	// Tally card suit
 	for (let i = 0; i < hand.length; i += 1) {
 		const cardSuit = hand[i].suit;
 		if (cardSuit in cardSuitTally) {
@@ -197,55 +260,82 @@ const checkForWin = (bet) => {
 			cardSuitTally[cardSuit] = 1;
 		}
 	}
-	console.log(cardSuitTally);
+};
 
-	// Royal flush (*800) - 10, J, Q, K, A
-	if (
-		Object.keys(cardNameTally).length === 5 &&
-		Object.keys(cardNameTally).includes('A') &&
-		Object.values(cardSuitTally).includes(5)
-	) {
+// Function to start a new game with same bet
+const newGame = () => {
+	deck.length = 0;
+	deck = shuffleCards(makeDeck());
+
+	hand.length = 0;
+	heldCardsIndex.length = 0;
+	updatePoints();
+	updateMessage(`Ready to play?`);
+	createEmptyCardElements();
+	dealBtn.disabled = false;
+};
+
+// ***** GAMEPLAY *****************************************
+
+// To check for win
+const checkForWin = () => {
+	let winAmount = 0;
+	let lose = false;
+	tallyCards();
+
+	/* If all cards have the same suit, could be
+	- Royal Flush
+	- Straight Flush
+	- Flush */
+	if (Object.values(cardSuitTally).includes(5)) {
 		const handCopy = [...hand];
 		handCopy.sort(function (a, b) {
 			return a.rank - b.rank;
 		});
 		let runningRank = 1;
-		for (let i = 1; i < handCopy.length - 1; i += 1) {
-			if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
-				runningRank += 1;
-			}
-		}
-		if (runningRank === 4) {
-			points += bet * 800;
-			console.log('ROYALLLLL FLUSHH!!!!!');
-		}
-	}
 
-	// Straight flush (*50) - straight but all same suit
-	else if (
-		Object.keys(cardNameTally).length === 5 &&
-		Object.values(cardSuitTally).includes(5)
-	) {
-		const handCopy = [...hand];
-		handCopy.sort(function (a, b) {
-			return a.rank - b.rank;
-		});
-		let runningRank = 1;
-		for (let i = 0; i < handCopy.length - 1; i += 1) {
-			if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
-				runningRank += 1;
+		if (
+			Object.keys(cardNameTally).includes('A') &&
+			Object.keys(cardNameTally).includes('K')
+		) {
+			for (let i = 1; i < handCopy.length - 1; i += 1) {
+				if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+					runningRank += 1;
+				}
+			}
+		} else {
+			for (let i = 0; i < handCopy.length - 1; i += 1) {
+				if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+					runningRank += 1;
+				}
 			}
 		}
-		if (runningRank === 5) {
-			points += bet * 50;
-			console.log('Straight FLUSHH~~');
+
+		// Royal flush (*800) - 10, J, Q, K, A
+		if (
+			Object.keys(cardNameTally).includes('A') &&
+			Object.keys(cardNameTally).includes('K') &&
+			runningRank === 4
+		) {
+			winAmount = bet * 800;
+			updateMessage('Royal flush');
+		}
+		// Straight flush (*50) - straight but all same suit
+		else if (runningRank === 5) {
+			winAmount = bet * 50;
+			updateMessage('Straight flush');
+		}
+		// Flush (*6) - all same suit
+		else {
+			winAmount = bet * 6;
+			updateMessage('Flush');
 		}
 	}
 
 	// Four of a kind (*25)
 	else if (Object.values(cardNameTally).includes(4)) {
 		points += bet * 25;
-		console.log('Four of a kind!');
+		updateMessage('Four of a kind');
 	}
 
 	// Full house (*9)
@@ -253,14 +343,8 @@ const checkForWin = (bet) => {
 		Object.values(cardNameTally).includes(2) &&
 		Object.values(cardNameTally).includes(3)
 	) {
-		points += bet * 9;
-		console.log('Full house!!');
-	}
-
-	// Flush (*6) - all same suit
-	else if (Object.values(cardSuitTally).includes(5)) {
-		points += bet * 6;
-		console.log('Flush!!');
+		winAmount = bet * 9;
+		updateMessage('Full house');
 	}
 
 	// Straight (*4)
@@ -270,21 +354,46 @@ const checkForWin = (bet) => {
 			return a.rank - b.rank;
 		});
 		let runningRank = 1;
-		for (let i = 0; i < handCopy.length - 1; i += 1) {
-			if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
-				runningRank += 1;
+
+		// For 10, J, Q, K, A straight
+		if (
+			Object.keys(cardNameTally).includes('A') &&
+			Object.keys(cardNameTally).includes('K')
+		) {
+			for (let i = 1; i < handCopy.length - 1; i += 1) {
+				if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+					runningRank += 1;
+				}
 			}
 		}
-		if (runningRank === 5) {
-			points += bet * 4;
-			console.log('Straight~~');
+		// For all other straights
+		else {
+			for (let i = 0; i < handCopy.length - 1; i += 1) {
+				if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+					runningRank += 1;
+				}
+			}
+		}
+
+		if (
+			(Object.keys(cardNameTally).includes('A') &&
+				Object.keys(cardNameTally).includes('K') &&
+				runningRank === 4) ||
+			runningRank === 5
+		) {
+			winAmount = bet * 4;
+			updateMessage('Straight');
+		}
+		// If not straight
+		else {
+			lose = true;
 		}
 	}
 
 	// Three of a kind (*3)
 	else if (Object.values(cardNameTally).includes(3)) {
-		points += bet * 3;
-		console.log('Three of a kind!');
+		winAmount = bet * 3;
+		updateMessage('Three of a kind');
 	}
 
 	// 2 pairs (*2)
@@ -292,8 +401,8 @@ const checkForWin = (bet) => {
 		Object.values(cardNameTally).includes(2) &&
 		Object.values(cardNameTally).length === 3
 	) {
-		points += bet * 2;
-		console.log('Two pairs!');
+		winAmount = bet * 2;
+		updateMessage('Two pairs');
 	}
 
 	// Jacks or better (*1)
@@ -303,11 +412,23 @@ const checkForWin = (bet) => {
 		cardNameTally['K'] === 2 ||
 		cardNameTally['A'] === 2
 	) {
-		points += bet * 1;
-		console.log('JACKS OR BETTER');
+		winAmount = bet * 1;
+		updateMessage('Jacks or better');
 	}
 
-	pointsContainer.innerText = points;
+	// Lose
+	else {
+		lose = true;
+	}
+
+	points += winAmount;
+	// Show winAmount next to points
+
+	if (lose) {
+		points -= bet;
+		updateMessage(`Better luck next round`);
+	}
+
 	createCardElement(hand);
 };
 
@@ -318,26 +439,46 @@ const initGame = () => {
 	deck = shuffleCards(makeDeck());
 
 	// Format divs
+	pointsContainer.setAttribute('id', 'points');
+	messageBoard.setAttribute('id', 'message-board');
 	dealBtn.setAttribute('id', 'deal-button');
 	cardContainer.setAttribute('id', 'card-container');
-	gameInstructions.classList.add('game-instructions');
-	pointsContainer.setAttribute('id', 'points');
-	mainContainer.classList.add('main');
+	gameInfoContainer.setAttribute('id', 'game-info');
+	gameInstructions.setAttribute('id', 'game-instructions');
+	payTable.setAttribute('id', 'pay-table');
+	mainContainer.setAttribute('id', 'main');
 
+	// Add content to the elements
+	const howToPlay = [
+		`How to play`,
+		`Click 'Deal' to draw 5 cards`,
+		`Select any cards you want to hold.`,
+		`The rest will be discarded.`,
+		`Click 'Draw' to replace discarded cards.`,
+	];
+	const ul = document.createElement('ul');
+	for (let i = 0; i < howToPlay.length; i += 1) {
+		const li = document.createElement('li');
+		li.innerText = howToPlay[i];
+		ul.appendChild(li);
+	}
+	gameInstructions.appendChild(ul);
+	gameInfoContainer.appendChild(gameInstructions);
+	updatePoints();
+	messageBoard.innerHTML = `Ready to play?`;
 	dealBtn.innerText = `Deal`;
-	cardContainer.innerHTML = ``;
-	gameInstructions.innerHTML = `Ready to play?`;
-	pointsContainer.innerText = points;
+	createEmptyCardElements();
 
 	dealBtn.addEventListener('click', () => {
 		dealCards();
 	});
 
 	// Append divs to doc
-	mainContainer.appendChild(gameInstructions);
 	mainContainer.appendChild(pointsContainer);
+	mainContainer.appendChild(messageBoard);
 	mainContainer.appendChild(dealBtn);
 	mainContainer.appendChild(cardContainer);
+	mainContainer.appendChild(gameInfoContainer);
 
 	document.body.appendChild(mainContainer);
 };
--- testhands.js
+++ testhands.js
@@ -7,11 +7,11 @@ const royalFlush = [
 ];
 
 const straightFlush = [
-	{ name: '9', suit: 'clubs', symbol: '♣️', color: 'black', rank: 9 },
-	{ name: '7', suit: 'clubs', symbol: '♣️', color: 'black', rank: 7 },
-	{ name: 'J', suit: 'clubs', symbol: '♣️', color: 'black', rank: 11 },
-	{ name: '10', suit: 'clubs', symbol: '♣️', color: 'black', rank: 10 },
-	{ name: '8', suit: 'clubs', symbol: '♣️', color: 'black', rank: 8 },
+	{ name: '4', suit: 'clubs', symbol: '♣️', color: 'black', rank: 4 },
+	{ name: '5', suit: 'clubs', symbol: '♣️', color: 'black', rank: 5 },
+	{ name: '3', suit: 'clubs', symbol: '♣️', color: 'black', rank: 3 },
+	{ name: 'A', suit: 'clubs', symbol: '♣️', color: 'black', rank: 1 },
+	{ name: '2', suit: 'clubs', symbol: '♣️', color: 'black', rank: 2 },
 ];
 
 const kind4 = [
@@ -46,6 +46,14 @@ const straight = [
 	{ name: '8', suit: 'clubs', symbol: '♣️', color: 'black', rank: 8 },
 ];
 
+const straightAce = [
+	{ name: 'Q', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 12 },
+	{ name: 'J', suit: 'clubs', symbol: '♣️', color: 'black', rank: 11 },
+	{ name: '10', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 10 },
+	{ name: 'K', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 13 },
+	{ name: 'A', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 1 },
+];
+
 const kind3 = [
 	{ name: 'Q', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 12 },
 	{ name: 'Q', suit: 'diamonds', symbol: '♦️', color: 'red', rank: 12 },
@@ -69,3 +77,54 @@ const pairJacks = [
 	{ name: 'K', suit: 'hearts', symbol: '♥️', color: 'red', rank: 13 },
 	{ name: '2', suit: 'hearts', symbol: '♥️', color: 'red', rank: 2 },
 ];
+
+/* Initial testing of the flushes (logical error)
+// Royal flush (*800) - 10, J, Q, K, A
+if (
+	Object.keys(cardNameTally).length === 5 &&
+	Object.keys(cardNameTally).includes('A') &&
+	Object.values(cardSuitTally).includes(5)
+) {
+	const handCopy = [...hand];
+	handCopy.sort(function (a, b) {
+		return a.rank - b.rank;
+	});
+	let runningRank = 1;
+	for (let i = 1; i < handCopy.length - 1; i += 1) {
+		if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+			runningRank += 1;
+		}
+	}
+	if (runningRank === 4) {
+		points += bet * 800;
+		console.log('ROYALLLLL FLUSHH!!!!!');
+	}
+}
+
+// Straight flush (*50) - straight but all same suit
+else if (
+	Object.keys(cardNameTally).length === 5 &&
+	Object.values(cardSuitTally).includes(5)
+) {
+	const handCopy = [...hand];
+	handCopy.sort(function (a, b) {
+		return a.rank - b.rank;
+	});
+	let runningRank = 1;
+	for (let i = 0; i < handCopy.length - 1; i += 1) {
+		if (handCopy[i].rank === handCopy[i + 1].rank - 1) {
+			runningRank += 1;
+		}
+	}
+	if (runningRank === 5) {
+		points += bet * 50;
+		console.log('Straight FLUSHH~~');
+	}
+}
+
+	// Flush (*6) - all same suit
+	else if (Object.keys(cardSuitTally).includes(5)) {
+		points += bet * 6;
+		console.log('Flush!!');
+	}
+*/
