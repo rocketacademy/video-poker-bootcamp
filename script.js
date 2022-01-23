1f87d72fd701d5e00edf1f7a4c207bc35fc2fa4c
wangzaa <aikkoku@yahoo.com>
2022-01-23 18:42:56 +0800

    updated code

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+/// ///////GLOBAL VARIABLES//////////
+let deck = [];
+const numberOfCards = 5;
+const gameScoreContainer = document.createElement('div');
+const gameScore = 100;
+const gameScoreStep = 20;
+const hand = [];
+let canClick = true;
+let fiveCardContainer;
+let cardContainer;
+let discardHand;
+let swapHand;
+
+// message container
+const messageContainer = document.createElement('div');
+messageContainer.classList.add('message');
+document.body.appendChild(messageContainer);
+
+// message output
+const messageOutput = document.createElement('span');
+messageContainer.append(messageOutput);
+messageOutput.innerText = '';
+
 /// ///////HELPER FUNCTIONS//////////
 // Create shuffled card deck
 const getRandomIndex = (max) => Math.floor(Math.random() * max);
@@ -44,91 +67,147 @@ const makeDeck = () => {
   // Return the completed card deck
   return newDeck;
 };
-// update create card display
-const createCard = (cardInfo) => {
-  const card = document.createElement('div');
-  card.classList.add('card');
 
-  const suit = document.createElement('div');
-  suit.innerText = cardInfo.suit;
+const buildFiveCardContainer = () => {
+  fiveCardContainer = document.getElementById('fiveCardContainer');
+  fiveCardContainer.innerHTML = '';
 
-  const name = document.createElement('div');
-  name.innerText = cardInfo.name;
-
-  // append card name and suit to the card div
-  card.appendChild(name);
-  card.appendChild(suit);
-  return card;
+  for (let i = 0; i < numberOfCards; i += 1) {
+    cardContainer = document.createElement('div');
+    cardContainer.classList.add('cardContainer');
+    cardContainer.setAttribute('id', `card${i}`);
+    fiveCardContainer.appendChild(cardContainer);
+  }
 };
 
-/// ///////GLOBAL VARIABLES//////////
-const deck = shuffleCards(makeDeck());
-const numberOfCards = 5;
-const player1Button = document.createElement('button');
-const gameInfo = document.createElement('div');
-let canClick = true;
-let cardContainerP1;
-const p1Hand = [];
-let playerHand;
+const enterSwapHand = () => {
+  discardHand = 
+  for (let i = 0; i < discardHand.length; i += 1) {
+    swapHand[i] = deck.pop();
+  }
+  console.log(discardHand);
+  console.log(swapHand);
+};
+
+const openHand = () => {
+  for (let i = 0; i < numberOfCards; i += 1) {
+    hand[i] = deck.pop();
+    cardContainer = document.getElementById(`card${i}`);
+    cardContainer.innerHTML = `${hand[i].name} <br> ${hand[i].suit}`;
+    cardContainer.classList.add('cardContainer');
+    cardContainer.addEventListener('click', enterSwapHand);
+  }
+};
 
 /// ////////////CALLBACK FUNCTIONS/////////
 
-const player1Click = () => {
+const analyseHand = () => {
+  const handSuit = hand.map(({ suit }) => suit);
+  const handRank = hand.map(({ rank }) => rank);
+
+  const ELE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  const ele = handRank;
+  const eleSuit = handSuit;
+
+  // testing ele, eleSuit
+  // const ele = [2, 2, 4, 5, 6];
+  // const eleSuit = ['♥', '♥', '♥', '♥', '♥'];
+
+  const flush = eleSuit.every((i) => i === eleSuit[0]);
+  const group = ELE.map((n, i) => ele.filter((j) => i === j).length).sort((x, y) => y - x);
+  console.log(group);
+
+  const distance = Math.max(...ele) - Math.min(...ele);
+  const straight = group[0] === 1 && distance < 5;
+
+  if (straight && flush) return 'straight-flush';
+  if (group[0] === 4) return 'four-of-a-kind';
+  if (group[0] === 3 && group[1] === 2) return 'full-house';
+  if (flush) return 'flush';
+  if (straight) return 'straight';
+  if (group[0] === 3) return 'three-of-a-kind';
+  if (group[0] === 2 && group[1] === 2) return 'two-pair';
+  if (group[0] === 2) return 'one-pair';
+  return 'You din catch anything';
+};
+
+const calScore = () => {
+  const result = analyseHand();
+
+  if (result === 'straight-flush') return Math.round(gameScoreStep / 0.0012);
+  if (result === 'four-of-a-kind') return Math.round(gameScoreStep / 0.0239);
+  if (result === 'full-house') return Math.round(gameScoreStep / 0.144);
+  if (result === 'flush') return Math.round(gameScoreStep / 0.19);
+  if (result === 'straight') return Math.round(gameScoreStep / 0.35);
+  if (result === 'three-of-a-kind') return Math.round(gameScoreStep / 0.5211);
+  if (result === 'two-pair') return Math.round(gameScoreStep / 0.747);
+  if (result === 'one-pair') return Math.round(gameScoreStep / 0.942);
+  if (result === 'You din catch anything') return '0';
+};
+
+const drawClick = () => {
   if (canClick === true) {
     canClick = false;
   }
-  for (let i = 0; i < numberOfCards; i += 1) {
-    p1Hand[i] = deck.pop();
-  }
-  console.log(p1Hand);
-  console.log(p1Hand.name);
-  console.log(p1Hand.suit);
+  messageOutput.innerText = '';
+  cardContainer.innerHTML = '';
+  deck = shuffleCards(makeDeck());
 
-  // p1Hand.sort((a, b) => a.rank - b.rank);
-  // setTimeout method to show cards one by one
-  let x = 0;
-  for (let i = 0; i < numberOfCards; i += 1) {
-    x += 500;
-    setTimeout(() => {
-      const cardElement = createCard(p1Hand[i]);
-      cardContainerP1.appendChild(cardElement);
-    }, x);
-  }
   canClick = true;
+  openHand();
+  setTimeout(() => {
+    messageOutput.innerText = `${analyseHand()}; Pick any card to swap!`;
+    gameScoreContainer.innerText = `${gameScore + calScore()}`;
+    console.log(`${calScore()}`);
+  }, 2500);
 };
 
-const analyzeHand = (hand) => {
-  const flush = card.suit.every((i) => i === card.suit[0]);
-  const groups = card.name.map((n, i) => card.name.filter((j) => i === j).length).sort((x, y) => y - x);
-  const distance = Math.max(...card.rank) - Math.min(...card.rank);
-  const straight = groups[0] === 1 && distance < 5;
-
-  if (straight && flush) return 'straight-flush';
-  if (groups[0] === 4) return 'four-of-a-kind';
-  if (groups[0] === 3 && groups[1] === 2) return 'full-house';
-  if (flush) return 'flush';
-  if (straight) return 'straight';
-  if (groups[0] === 3) return 'three-of-a-kind';
-  if (groups[0] === 2 && groups[1] === 2) return 'two-pair';
-  if (groups[0] === 2) return 'one-pair';
-  return 'high-card';
+const swapClick = () => {
+  enterSwapHand();
+  swapHand.sort((a, b) => a - b);
+  for (let i = 0; i < swapHand.length; i += 1) {
+    cardContainer = document.getElementById(`card${swapHand[i]}`);
+    cardContainer.innerHTML = `${swapHand[i].name}${swapHand[i].suit}`;
+    cardContainer.classList.add('cardContainer');
+  }
 };
-// for (hand of playerHand) console.log(`${hand}: ${analyzeHand(hand)}`);
+
+const resetClick = () => {
+  initGame();
+};
 
 /// ////////////INITISATION///////////////
 const initGame = () => {
-  // player 1 draw button
-  player1Button.innerText = 'Draw Cards';
-  document.body.appendChild(player1Button);
-  player1Button.addEventListener('click', player1Click);
-
   // fill game info div with starting instructions
-  gameInfo.innerText = 'Click to draw a card!';
-  document.body.appendChild(gameInfo);
+  messageOutput.innerText = 'Click to draw a card!';
 
-  // initialise a div 'cardContainer'
-  cardContainerP1 = document.createElement('div');
-  cardContainerP1.classList.add('cardContainer');
-  document.body.appendChild(cardContainerP1);
+  // fill game score div with score
+  gameScoreContainer.innerText = gameScore;
+  document.body.appendChild(gameScoreContainer);
+
+  // initialise div fivecardContainer'
+  const fiveCardContainer = document.createElement('div');
+  fiveCardContainer.setAttribute('id', 'fiveCardContainer');
+  document.body.appendChild(fiveCardContainer);
+
+  // prepare 3 buttons for draw, swap, reset
+  const drawButton = document.createElement('button');
+  drawButton.innerText = 'DRAW';
+  document.body.appendChild(drawButton);
+  drawButton.addEventListener('click', drawClick);
+
+  const swapButton = document.createElement('button');
+  swapButton.innerText = 'SWAP';
+  document.body.appendChild(swapButton);
+  swapButton.addEventListener('click', swapClick);
+
+  const resetButton = document.createElement('button');
+  resetButton.innerText = 'RESET';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetClick);
+
+  // build card container to hold five cards
+  buildFiveCardContainer();
 };
 initGame();
