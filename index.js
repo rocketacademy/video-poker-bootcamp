da53e9f431e48565e68280d3d3441bedf7c3ed53
berniceyeo <berniceyeosuping@gmail.com>
2022-07-06 15:33:53 +0800

    add express

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-import { createServer } from "http";
-import { readFile } from "fs";
+import express from "express";
+import { resolve } from "path";
 
-const handleIncomingRequest = (request, response) => {
-  // request.url contains the portion of the URL after the domain.
-  // E.g. for https://ra.co/index.html, request.url would return "/index.html".
-  // console.log("request url", request.url);
+const app = express();
+const PORT = 3004;
 
-  // // "." refers to the Unix filesystem ".", which represents the current directory.
-  // const filePath = "." + request.url;
-
-  readFile("./index.html", (err, content) => {
-    if (err) {
-      console.error("error reading file", err);
-      return;
-    }
-    // Set the response code to 200 (i.e. OK)
-    response.writeHead(200);
-    // Send the response with the file content in utf-8 format
-    response.end(content, "utf-8");
-  });
-};
-
-// Initialise server with request listener function handleIncomingRequest
-// https://nodejs.org/api/http.html#http_http_createserver_options_requestlistener
-// Use port 3004 by convention.
-createServer(handleIncomingRequest).listen(3004);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(resolve("./index.html"));
+});
+app.listen(3004);
